5a2a3933557424e05138983fffc32129eaf67196
Kashiif-Khan <kashifafridi243@gmail.com>
2025-11-27 17:25:57 +0500

    frontend folder structure

---
+++ Frontend/smart-society-service-system/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-red-500 font-extrabold text-[50px] flex justify-center py-60 '>Wellcome to fyp</h1>
+    </>
+  )
+}
+
+export default App
